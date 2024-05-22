f71664d1388fd4f097efc363ad9c4f64ee0505e1
einstein-gucci <avdijigm@students.zhaw.ch>
2024-05-22 14:34:57 +0200

    ye

---
+++ src/main/java/ch/zhaw/techland/TechlandApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.techland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlandApplication.class, args);
+	}
+
+}
